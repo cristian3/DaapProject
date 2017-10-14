25a9bfe4d1f2e14b97206577405a7ed552cea30f
Cristian Javier Navia Santos <cristiannavia3@hotmail.com>
2017-10-14 20:02:56 +0200

    arquitectura

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Layouts
+import { FullLayoutComponent } from './layouts/full-layout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: FullLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
